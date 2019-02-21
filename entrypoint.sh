c1c65fb3b033b287d26dd9614111b5be3506f19b
Tom Brightbill <tbrightbill@cloudflare.com>
2019-02-21 15:44:51 -0800

    updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-
-
 mv $GITHUB_WORKSPACE/*.js /root/worker-deploy
 
 cd /root/worker-deploy
-
-echo 'switching to worker directory contents below'
-
-ls
-
-cat serverless.yml
-
-export SLS_DEBUG=*
 
 sls deploy
 
