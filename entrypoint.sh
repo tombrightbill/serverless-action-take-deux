ae83eb3f0120bd5e5647661d4b4c91ddef5b0dbb
Tom Brightbill <tbrightbill@cloudflare.com>
2019-02-21 13:22:35 -0800

    updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,13 @@
 #!/bin/bash
 
-echo 'I am'
-whoami
 
-echo "current directory is"
 
-pwd
+mv $GITHUB_WORKSPACE/*.js /root/deploy-worker
 
-echo "current content is"
-
-ls
-
-# echo "changing to /github/workspace/worker-deploy"
-
-# cd /github/workspace/worker-deploy/
-
-echo "current directory is blah"
-
-pwd
-
-echo "listing contents of this directory"
-
-ls
-
-echo 'github workspace is'
-
-echo $GITHUB_WORKSPACE
-
-echo ' HOME in action below'
-
-echo $HOME
+cd /root/deploy-worker
 
 echo 'switching to /root'
 
-cd /root
-ls
-pwd
+sls deploy
+
 
