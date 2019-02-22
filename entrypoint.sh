f66ef414b77617e1fda427ecb5850d60813a9ad6
Tom Brightbill <tbrightbill@cloudflare.com>
2019-02-22 15:05:59 -0800

    updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-mv $GITHUB_WORKSPACE/*.js /root/worker-deploy
+ACTION_WORKSPACE = /root/worker-deploy
 
-cd /root/worker-deploy
+if [ -e $GITHUB_WORKSPACE/serverless.yml ]
+then
+    mv $GITHUB_WORKSPACE/serverless.yml $ACTION_WORKSPACE
+fi
+
+mv $GITHUB_WORKSPACE/*.js $ACTION_WORKSPACE/worker-deploy
+
+cd $ACTION_WORKSPACE
 
 sls deploy
 
