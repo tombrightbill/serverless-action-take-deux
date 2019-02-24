469c0f7796f58f71b0de4966c135448922a48186
Tom Brightbill <tbrightbill@cloudflare.com>
2019-02-24 09:59:19 -0800

    updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,12 @@ ACTION_WORKSPACE=/root/worker-deploy
 
 if [ -e $GITHUB_WORKSPACE/serverless.yml ]
 then
+	echo 'Custom serverless.yml found.'
     mv $GITHUB_WORKSPACE/serverless.yml $ACTION_WORKSPACE
 fi
 
 mv $GITHUB_WORKSPACE/*.js $ACTION_WORKSPACE
 
-cd $ACTION_WORKSPACE
-
-sls deploy
+cd $ACTION_WORKSPACE && sls deploy
 
 
